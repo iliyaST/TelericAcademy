3a0d4b922f2d61230f10d29369638849cdf8bcb1
iliyaST <iliamartinovstambolov@gmail.com>
2016-08-13 01:42:42 +0300

    C#1

---
+++ CSharp1/8-Testing-Exam-two-23 June 2013/05.NeuronsSolvedBit/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class NeuronsBitVersion
+{
+    static void Main()
+    {
+        while (true)
+        {
+            long line = long.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(line, 2).PadLeft(32, '0'));
+            if (line == -1)
+            {
+                break;
+            }
+            int mostRightBit = -1;
+            int mostLeftBit = -2;
+            for (int p = 0; p < 32; p++)
+            {
+                int mask = 1 << p;
+                long nAndMask = line & mask;
+                long bit = nAndMask >> p;
+
+                if (bit == 1)
+                {
+                    if (mostRightBit == -1)
+                    {
+                        mostRightBit = p;
+                    }
+                    mostLeftBit = p;
+                }
+            }
+            Console.WriteLine("left {0} - right {1}",mostLeftBit,mostRightBit);
+            long result = 0;
+            for (int p = mostRightBit; p <= mostLeftBit; p++)
+            {
+                long mask = 1 << p;
+                long nAndMask = line & mask;
+                long bit = nAndMask >> p;
+                Console.WriteLine(Convert.ToString(nAndMask, 2).PadLeft(32, '0'));
+                if (bit == 0)
+                {
+                    result = result | mask;
+                }
+            }
+            Console.WriteLine(Convert.ToString(result, 2).PadLeft(32, '0'));
+        }
+    }
+}
+
