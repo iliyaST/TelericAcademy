d591db23f45a9d71438b10b33ac6d2d44d4dc583
iliyaST <iliamartinovstambolov@gmail.com>
2016-08-18 00:00:44 +0300

    10-Testing-Exam-Four-5December2013Evening

---
+++ CSharp1/10-Testing-Exam-Four-5December2013Evening/03.OnesAndZeros/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+class OnesAndZeros
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string inputNumber = Convert.ToString(n, 2).PadLeft(16, '0');
+        string zero = "###\n#.#\n#.#\n#.#\n###";
+        string one = ".#.\n##.\n.#.\n.#.\n###";
+
+
+        for (int i = 0; i < inputNumber.Length; i++)
+        {
+            if(i==inputNumber.Length-1)
+            {
+                if (inputNumber[i] == '0')
+                {
+                    Console.Write("###");
+                }
+                if (inputNumber[i] == '1')
+                {
+                    Console.Write(".#.");
+                }
+                continue;
+            }
+            if (inputNumber[i] == '0')
+            {
+                Console.Write("###.");
+            }
+            if (inputNumber[i] == '1')
+            {
+                Console.Write(".#..");
+            }
+        }
+        Console.WriteLine();
+        for (int i = 0; i < inputNumber.Length; i++)
+        {
+            if (i == inputNumber.Length - 1)
+            {
+                if (inputNumber[i] == '0')
+                {
+                    Console.Write("#.#");
+                }
+                if (inputNumber[i] == '1')
+                {
+                    Console.Write("##.");
+                }
+                continue;
+            }
+            if (inputNumber[i] == '0')
+            {
+                Console.Write("#.#.");
+            }
+            if (inputNumber[i] == '1')
+            {
+                Console.Write("##..");
+            }
+        }
+        Console.WriteLine();
+        for (int i = 0; i < inputNumber.Length; i++)
+        {
+            if (i == inputNumber.Length - 1)
+            {
+                if (inputNumber[i] == '0')
+                {
+                    Console.Write("#.#");
+                }
+                if (inputNumber[i] == '1')
+                {
+                    Console.Write(".#.");
+                }
+                continue;
+            }
+            if (inputNumber[i] == '0')
+            {
+                Console.Write("#.#.");
+            }
+            if (inputNumber[i] == '1')
+            {
+                Console.Write(".#..");
+            }
+        }
+        Console.WriteLine();
+        for (int i = 0; i < inputNumber.Length; i++)
+        {
+            if (i == inputNumber.Length - 1)
+            {
+                if (inputNumber[i] == '0')
+                {
+                    Console.Write("#.#");
+                }
+                if (inputNumber[i] == '1')
+                {
+                    Console.Write(".#.");
+                }
+                continue;
+            }
+            if (inputNumber[i] == '0')
+            {
+                Console.Write("#.#.");
+            }
+            if (inputNumber[i] == '1')
+            {
+                Console.Write(".#..");
+            }
+        }
+        Console.WriteLine();
+        for (int i = 0; i < inputNumber.Length; i++)
+        {
+            if (i == inputNumber.Length - 1)
+            {
+                if (inputNumber[i] == '0')
+                {
+                    Console.Write("###");
+                }
+                if (inputNumber[i] == '1')
+                {
+                    Console.Write("###");
+                }
+                continue;
+            }
+            if (inputNumber[i] == '0')
+            {
+                Console.Write("###.");
+            }
+            if (inputNumber[i] == '1')
+            {
+                Console.Write("###.");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ CSharp1/10-Testing-Exam-Four-5December2013Evening/04.Eggcelent/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+class Eggcelent
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write(new string('.', n + 1));
+        Console.Write(new string('*', n - 1));
+        Console.WriteLine(new string('.', n + 1));
+        for (int i = 0; i < n / 2 - 1; i++)
+        {
+            Console.Write(new string('.', n - 1 - (i * 2)));
+            Console.Write(new string('*', 1));
+            Console.Write(new string('.', ((((3 * n) - 1) - 2) - (2 * (n - 1 - (i * 2)))) + 2));
+            Console.Write(new string('*', 1));
+            Console.WriteLine(new string('.', n - 1 - (i * 2)));
+        }
+        if (n >= 4)
+        {
+            for (int i = 0; i < n / 2 - 1; i++)
+            {
+                Console.Write(new string('.', 1));
+                Console.Write(new string('*', 1));
+                Console.Write(new string('.', (n * 3 - 3)));
+                Console.Write(new string('*', 1));
+                Console.WriteLine(new string('.', 1));
+            }
+        }
+        Console.Write(new string('.', 1));
+        Console.Write(new string('*', 1));
+        for (int i = 0; i < (n * 3 - 3) / 2; i++)
+        {
+            Console.Write(new string('@', 1));
+            Console.Write(new string('.', 1));
+        }
+        Console.Write(new string('@', 1));
+        Console.Write(new string('*', 1));
+        Console.WriteLine(new string('.', 1));
+        Console.Write(new string('.', 1));
+        Console.Write(new string('*', 1));
+        for (int i = 0; i < (n * 3 - 3) / 2; i++)
+        {
+            Console.Write(new string('.', 1));
+            Console.Write(new string('@', 1));
+        }
+        Console.Write(new string('.', 1));
+        Console.Write(new string('*', 1));
+        Console.WriteLine(new string('.', 1));
+        if (n >= 4)
+        {
+            for (int i = 0; i < n / 2 - 1; i++)
+            {
+                Console.Write(new string('.', 1));
+                Console.Write(new string('*', 1));
+                Console.Write(new string('.', (n * 3 - 3)));
+                Console.Write(new string('*', 1));
+                Console.WriteLine(new string('.', 1));
+            }
+        }
+        for (int i = 0; i < n / 2 - 1; i++)
+        {
+            Console.Write(new string('.', 3 + (i * 2)));
+            Console.Write(new string('*', 1));
+            Console.Write(new string('.', ((n * 3 - 4) - ((3 + (i * 2)*2)))));
+            Console.Write(new string('*', 1));
+            Console.WriteLine(new string('.', 3+ (i * 2)));
+        }
+        Console.Write(new string('.', n + 1));
+        Console.Write(new string('*', n - 1));
+        Console.WriteLine(new string('.', n + 1));
+    }
+}
+
+
+
