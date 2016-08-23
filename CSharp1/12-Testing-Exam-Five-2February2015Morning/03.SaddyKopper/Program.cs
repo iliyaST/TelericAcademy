c139f278f32d5306abca018d5d3ad6dfe3c98b72
iliyaST <iliamartinovstambolov@gmail.com>
2016-08-24 02:40:21 +0300

    12-Testing-Exam-Five-2February2015Morning

---
+++ CSharp1/12-Testing-Exam-Five-2February2015Morning/03.SaddyKopper/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+using System.IO;
+
+class SaddyKopper
+{
+    static void Main()
+    {
+        StreamReader sr = new StreamReader("../../input.txt");
+        Console.SetIn(sr);
+
+        string randomNumber = Console.ReadLine();
+        long result = 0;
+        BigInteger product = 1;
+        int counter = 0;
+        string copyNumber = "";
+        int countTransformations = 0;
+
+        while (countTransformations < 10)
+        {
+            for (int i = 0; i < randomNumber.Length - 1 - counter; i++)
+            {
+                copyNumber += randomNumber[i].ToString();
+                if (i % 2 == 0)
+                {
+                    result += randomNumber[i] - '0';
+                }
+            }
+            counter++;
+            product *= result;
+            result = 0;
+            copyNumber = "";
+            if (counter == randomNumber.Length - 1)
+            {
+                product *= result;
+                Console.WriteLine(product);
+                countTransformations++;
+                break;
+            }
+        }
+    }
+}
+
