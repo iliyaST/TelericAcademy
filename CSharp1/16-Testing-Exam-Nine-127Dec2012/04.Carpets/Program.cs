5ae0d992fd95c28eb14c9ced74713f989bea860e
iliyaST <iliamartinovstambolov@gmail.com>
2016-08-30 00:32:40 +0300

    16-Testing-Exam-Nine-127Dec2012

--- CSharp1/16-Testing-Exam-Nine-127Dec2012/04.Carpets/Program.cs
+++ CSharp1/16-Testing-Exam-Nine-127Dec2012/04.Carpets/Program.cs
@@ -5,9 +5,107 @@ class Carpet
     static void Main()
     {
         int N = int.Parse(Console.ReadLine());
-        int elementsOnLines = N;
+        int counter = 0;
 
-        
+        //top
+        while (counter != N/2)
+        {
+            Console.Write(new string('.', N / 2 - 1 - counter));
+            for (int i = 0; i <= counter; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("/");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            if (counter % 2 == 0)
+            {
+                for (int x = 0; x <= counter; x++)
+                {
+                    if (x % 2 == 0)
+                    {
+                        Console.Write("\\");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+            else
+            {
+
+                for (int y = 0; y <= counter; y++)
+                {
+                    if (y % 2 == 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("\\");
+                    }
+                }
+            }
+            Console.Write(new string('.', N / 2 - 1 - counter));
+            Console.WriteLine();
+            counter++;
+        }
+        counter = N / 2;
+
+        //bottom
+        while (counter != 0)
+        {
+            Console.Write(new string('.', N / 2 - counter));
+            for (int i = 0; i < counter; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("\\");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            if (counter % 2 == 0)
+            {
+                for (int z = 0; z < counter; z++)
+                {
+                    if (z % 2 == 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("/");
+                    }
+                }
+            }
+            else
+            {
+                for (int t = 0; t < counter; t++)
+                {
+                    if (t % 2 == 0)
+                    {
+                        Console.Write("/");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+            Console.Write(new string('.', N / 2 - counter));
+            counter--;
+            Console.WriteLine();
+        }
+
     }
 }
+
 
