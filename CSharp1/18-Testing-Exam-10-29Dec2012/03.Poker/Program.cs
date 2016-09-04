29397b22e903c638ee18751df4be6eb3d3eaef69
iliyaST <iliamartinovstambolov@gmail.com>
2016-09-04 22:22:47 +0300

    18-Testing-Exam-10-29Dec2012

--- CSharp1/18-Testing-Exam-10-29Dec2012/03.Poker/Program.cs
+++ CSharp1/18-Testing-Exam-10-29Dec2012/03.Poker/Program.cs
@@ -10,12 +10,49 @@ class Poker
         int counter3 = 0;
         int counter4 = 0;
         int counter5 = 0;
+        int[] value = new int[5];
+        int[] value1 = new int[5];
+        int value1Counter = 0;
+        int min = int.MaxValue;
 
         for (int i = 0; i < 5; i++)
         {
             string temp = Console.ReadLine();
+            if (temp == "10")
+            {
+                temp = "+";
+                value[i] = 10;
+                hand += temp;
+                continue;
+            }
+            if (temp == "J")
+            {
+                value[i] = 11;
+                hand += temp;
+                continue;
+            }
+            if (temp == "Q")
+            {
+                value[i] = 12;
+                hand += temp;
+                continue;               
+            }
+            if (temp == "K")
+            {
+                value[i] = 13;
+                hand += temp;
+                continue;              
+            }
+            if (temp == "A")
+            {
+                value[i] = 14;
+                hand += temp;
+                continue;              
+            }
+            value[i] = Convert.ToInt32(temp);
             hand += temp;
         }
+
         string card1 = hand[0].ToString();
         string card2 = hand[1].ToString();
         string card3 = hand[2].ToString();
@@ -24,21 +61,21 @@ class Poker
 
         for (int i = 0; i < 5; i++)
         {
-            if (hand[i].ToString()==card1)
+            if (hand[i].ToString() == card1)
             {
                 counter1++;
-            }          
+            }
         }
-        string a = hand.Replace(card1, "*"); 
+        string a = hand.Replace(card1, "*");
 
         for (int i = 0; i < 5; i++)
         {
             if (card2 == a[i].ToString())
             {
                 counter2++;
-            }          
+            }
         }
-        string b = hand.Replace(card2,"$");
+        string b = a.Replace(card2, "$");
 
         for (int i = 0; i < 5; i++)
         {
@@ -47,7 +84,7 @@ class Poker
                 counter3++;
             }
         }
-        string c = hand.Replace(card3, "@");
+        string c = b.Replace(card3, "@");
         for (int i = 0; i < 5; i++)
         {
             if (card4 == c[i].ToString())
@@ -56,17 +93,17 @@ class Poker
                 hand.Replace('%', hand[i]);
             }
         }
-        string d = hand.Replace(card4, "%");
+
+        string d = c.Replace(card4, "%");
         for (int i = 0; i < 5; i++)
         {
-            if (card4 == hand[i].ToString())
+            if (card5 == d[i].ToString())
             {
                 counter5++;
-                hand.Replace('&',hand[i]);
+                hand.Replace('&', hand[i]);
             }
         }
-        string e = hand.Replace(card4, "&");
-        
+
 
         if (counter1 == 5 || counter2 == 5 || counter3 == 5 || counter4 == 5 || counter5 == 5)
         {
@@ -102,12 +139,51 @@ class Poker
                 Console.WriteLine("Two Pairs");
             }
         }
-        else
+        else if (counter1 == 1 && counter2 == 1 && counter3 == 1 && counter4 == 1 && counter5 == 1)
         {
-            Console.WriteLine("Nothing");
-        }
-
-
+            if (value[0] + value[1] + value[2] + value[3] + value[4] < 60)
+            {               
+                for (int i = 0; i < 5; i++)
+                {
+                    value1[i] = value[i];
+                    if (value[i] == 14)
+                    {
+                        value1[i] = value[i] - 13;
+                    }
+                }                
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    value1[i] = value[i];                  
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                if(min>value1[i])
+                {
+                    min = value1[i];
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                if(value1[i]==min+1)
+                {
+                    min++;
+                    value1Counter++;
+                    i = -1;
+                }
+            }          
+            if(value1Counter==4)
+            {
+                Console.WriteLine("Straight");
+            } 
+            else
+            {
+                Console.WriteLine("Nothing");
+            }
+        }       
     }
 }
 
