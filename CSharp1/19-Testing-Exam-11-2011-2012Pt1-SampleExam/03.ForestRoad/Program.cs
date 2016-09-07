e3f6972166a5d448b0d686e575ce215329fc9711
iliyaST <iliamartinovstambolov@gmail.com>
2016-09-07 22:47:03 +0300

    19-Testing-Exam-11-2011-2012Pt1-SampleExam

---
+++ CSharp1/19-Testing-Exam-11-2011-2012Pt1-SampleExam/03.ForestRoad/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+class ForestRoad
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int posStar = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (posStar == i && posStar == j)
+                {
+                    Console.Write("*");
+                    posStar++;
+                    continue;
+                }
+                Console.Write(".");
+            }
+            Console.WriteLine();
+        }
+        for (int i = N - 2; i >= 0; i--)
+        {
+            posStar = 0;
+            for (int j = 0; j < N; j++)
+            {
+                if (posStar == i)
+                {
+                    Console.Write("*");
+                    posStar++;
+                }
+                else
+                {
+                    Console.Write(".");
+                    posStar++;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ CSharp1/19-Testing-Exam-11-2011-2012Pt1-SampleExam/05.SubsetSums/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+class SubsetSumbs
+{
+    static void Main()
+    {
+        BigInteger S = BigInteger.Parse(Console.ReadLine());
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            BigInteger temp = BigInteger.Parse(Console.ReadLine());
+
+        }
+    }
+}
