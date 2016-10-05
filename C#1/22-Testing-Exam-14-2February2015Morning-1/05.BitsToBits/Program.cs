6d5c34391656303e58bb4cce3b091a7d36b0bdc5
iliyaST <iliamartinovstambolov@gmail.com>
2016-10-05 10:51:50 +0300

    BitsToBits

---
+++ C#1/22-Testing-Exam-14-2February2015Morning-1/05.BitsToBits/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class BitsToBits
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int lastBit = -1;
+        int maxZeroes = 0;
+        int maxOnes = 0;
+        int counterOnes = 0;
+        int counterZeroes = 0;
+
+
+        for (int i = 0; i < N; i++)
+        {
+            int temp = int.Parse(Console.ReadLine());
+
+            for (int j = 29; j >= 0; j--)
+            {
+                int mask = 1 << j;
+                int nAndMask = temp & mask;
+                int bit = nAndMask >> j;
+
+                if (bit == 1)
+                {
+                    if (counterZeroes > maxZeroes)
+                    {
+                        maxZeroes = counterZeroes;
+                    }
+                    counterZeroes = 0;
+                    counterOnes++;
+                }
+                else if (bit == 0)
+                {
+                    if (counterOnes > maxOnes)
+                    {
+                        maxOnes = counterOnes;
+                    }
+                    counterOnes = 0;
+                    counterZeroes++;
+                }
+            }
+        }
+
+        if (counterOnes > maxOnes)
+        {
+            maxOnes = counterOnes;
+        }
+        if (counterZeroes > maxZeroes)
+        {
+            maxZeroes = counterZeroes;
+        }
+
+        Console.WriteLine(maxZeroes);
+        Console.WriteLine(maxOnes);
+    }
+}
+
