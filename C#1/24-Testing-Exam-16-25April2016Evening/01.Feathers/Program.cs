775f52b073c39a5546837babe1af5b390af24cd4
iliyaST <iliamartinovstambolov@gmail.com>
2016-10-06 16:30:38 +0300

    24-Testing-Exam-16-25April2016Evening

---
+++ C#1/24-Testing-Exam-16-25April2016Evening/01.Feathers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Feathers
+{
+    static void Main()
+    {
+        int B = int.Parse(Console.ReadLine());
+        int F = int.Parse(Console.ReadLine());
+
+        double averege = F / (double)B;
+        double result = 0;
+
+        if (B % 2 == 0)
+        {
+            result = averege * 123123123123.0;
+        }
+        else
+        {
+            result = averege / 317.0;
+        }
+
+        Console.WriteLine("{0:F4}",result);
+    }
+}
+
---
+++ C#1/24-Testing-Exam-16-25April2016Evening/02.Busses/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Busses
+{
+    static void Main()
+    {
+        int C = int.Parse(Console.ReadLine());
+        int[] speeds = new int[C];
+        int groupsCount = 0;
+
+        for (int i = 0; i < C; i++)
+        {
+            int S = int.Parse(Console.ReadLine());
+            speeds[i] = S;
+        }
+
+        for (int i = 0; i < C; i++)
+        {
+            if ((i == C - 1) && speeds[i - 1] >= speeds[i])
+            {
+                groupsCount++;
+                break;
+            }
+            if (speeds[i + 1] > speeds[i])
+            {
+                speeds[i + 1] = speeds[i];
+            }
+            else
+            {
+                groupsCount++;
+            }
+        }
+
+        Console.WriteLine(groupsCount);
+    }
+}
+
