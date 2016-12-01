f3497a34ef9c3ac91bdbcb3733ab9c98703152ed
iliyaST <iliamartinovstambolov@gmail.com>
2016-12-01 22:25:42 +0200

    13-TestExam-Few-Tasks-2016

---
+++ C#2/13-TestExam-Few-Tasks-2016/01.CryptoCS/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace CryptoCS
+{
+    class CryproCS
+    {    
+
+        static void Main()
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string text = Console.ReadLine();
+            char operat = char.Parse(Console.ReadLine());
+            string number = Console.ReadLine();
+            BigInteger index = 0;
+            BigInteger sum = 0;
+            BigInteger sum2 = 0;
+            BigInteger result = 0;
+            string resultStr = String.Empty;
+            string res = String.Empty;
+
+            int pow = text.Length - 1;
+
+            for (int i = 0; i <= text.Length - 1; i++)
+            {
+                for (int j = 0; j <= alphabet.Length - 1; j++)
+                {
+                    if (text[i] == alphabet[j])
+                    {
+                        sum += j * BigInteger.Pow(26, pow);
+                        pow--;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i <= number.Length - 1; i++)
+            {
+                sum2 += (number[i] - '0') * BigInteger.Pow(7, number.Length - 1 - i);
+            }
+
+            if (operat == '+')
+            {
+                result = sum + sum2;
+            }
+            else if (operat == '-')
+            {
+                result = sum - sum2;
+            }
+            resultStr = result.ToString();
+            while (result > 0)
+            {
+                res += result % 9;
+                result = result / 9;
+            }
+            char[] reversedResult = res.ToArray();
+            Array.Reverse(reversedResult);
+            Console.Write(String.Join("", reversedResult));
+        }
+    }
+}
---
+++ C#2/13-TestExam-Few-Tasks-2016/02.DancingMoves/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace DancingMoves
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int length = numbers.Length;
+            List<string> list = new List<string>();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "stop")
+                {
+                    break;
+                }
+                else
+                {
+                    list.Add(line);
+                }
+            }
+
+            double sum = 0;
+            int position = 0;
+            double result = 0;
+            for (int i = 0; i <= list.Count - 1; i++)
+            {
+                string[] currentDescr = list[i].Split(' ').ToArray();
+                int moves = int.Parse(currentDescr[0]);
+                string direction = currentDescr[1].ToString();
+                int jumps = int.Parse(currentDescr[2]);
+
+                for (int j = 0; j <= moves - 1; j++)
+                {
+                    if (direction == "right")
+                    {
+                        position += jumps;
+                        if (position > numbers.Length - 1)
+                        {
+                            position = position % length;
+                        }
+                        sum += numbers[position];
+                    }
+                    else if (direction == "left")
+                    {
+                        position -= jumps;
+                        if (position < 0)
+                        {
+                            position = -position;
+
+                            position = length - (position % numbers.Length);
+
+                        }
+                        sum += numbers[position];
+                    }
+                }
+            }
+            result = sum / 4;
+            Console.WriteLine("{0:F1}", result);
+        }
+    }
+}
