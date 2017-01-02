6ad6f1b7723fe42b993d0e11186586407e453dca
iliyaST <iliamartinovstambolov@gmail.com>
2017-01-02 17:49:40 +0200

    TestExam - 05-March-2014-Evening

---
+++ OOP/7-Exams/05-March-2014-Evening/01.Furniture/FurnitureManufacturer/Models/Furniture.cs
@@ -0,0 +1,71 @@
+﻿
+namespace FurnitureManufacturer.Models
+{
+    using System;
+    using FurnitureManufacturer.Interfaces;
+
+    public abstract class Furniture : IFurniture
+    {
+        private string model;
+        private decimal price;
+        private decimal height;
+
+        public virtual string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Model cannot be empty, null");
+                }
+
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Model cannot be with less than 3 symbols");
+                }
+
+                this.model = value;
+            }
+        }
+
+        public string Material { get; protected set; }
+
+        public virtual decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price cannot be less or equal to $0.00");
+                }
+
+                this.price = value;
+            }
+        }
+
+        public virtual decimal Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be less or equal to 0.00 m");
+                }
+            }
+        }
+
+        protected MaterialType MaterialType { get; set; }
+    }
+}
---
+++ OOP/7-Exams/05-March-2014-Evening/01.Furniture/FurnitureManufacturer/Models/Table.cs
@@ -0,0 +1,36 @@
+﻿
+namespace FurnitureManufacturer.Models
+{
+    using System;
+    using FurnitureManufacturer.Interfaces;
+
+    public class Table : Furniture, ITable
+    {
+        private decimal height;
+        private string materialType;
+        private string model;
+        private decimal price;
+
+        public Table(string model, MaterialType materialType, decimal price, decimal height, decimal length, decimal width)
+        {
+            this.Model = model;
+            this.MaterialType = materialType;
+            this.Price = price;
+            this.Height = height;
+            this.Length = length;
+            this.Width = width;
+        }
+
+        public decimal Area
+        {
+            get
+            {
+                return Length * Width;
+            }
+        }
+
+        public decimal Length { get; set; }
+
+        public decimal Width { get; set; }
+    }
+}
