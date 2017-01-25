713d594a2af6303002e34f1aaf198f8b4a4e3ee8
iliyaST <iliamartinovstambolov@gmail.com>
2017-01-25 19:00:58 +0200

    1-Unit-Testing

---
+++ KPK-Part-I-Unit-Test/1-Unit-Testing/School/ICourse.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+
+namespace School
+{
+    public interface ICourse
+    {
+        ICollection<Student> Students { get; set; }
+        void Add(Student student);
+        void Remove(Student student);
+    }
+}
---
+++ KPK-Part-I-Unit-Test/1-Unit-Testing/School/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace School
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
