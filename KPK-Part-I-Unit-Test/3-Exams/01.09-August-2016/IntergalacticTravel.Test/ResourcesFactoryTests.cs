6b823c1cf1b6872865b75d5e660ac358f7c5d1b4
iliyaST <iliamartinovstambolov@gmail.com>
2017-02-10 15:06:06 +0200

    Unit-Testing

---
+++ KPK-Part-I-Unit-Test/3-Exams/01.09-August-2016/IntergalacticTravel.Test/ResourcesFactoryTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace IntergalacticTravel.Test
+{
+    [TestClass]
+    public class ResourcesFactoryTests
+    {
+        [TestMethod]
+        [DataRow("gold(20)", "silver(30)", "bronze(40)")]
+        [DataRow("gold(20)", "bronze(40)", "silver(30)")]
+        [DataRow("silver(30)", "bronze(40)", "gold(20)")]
+        [DataRow("silver(30)", "gold(20)", "bronze(40)")]
+        [DataRow("bronze(40)", "gold(20)", "silver(30)")]
+        [DataRow("bronze(40)", "silver(30)", "gold(20)")]
+        public void GetResources_ShouldReturnANewlyCreatedResourcesObject_WithCorrectlySetUpProperties
+            (string firstProp, string secondProp, string thirdProp)
+        {
+            var factory = new ResourcesFactory();
+
+            var getResources = factory.GetResources($"create resources {firstProp} {secondProp} {thirdProp}");
+
+            Assert.IsInstanceOfType(getResources, typeof(Resources));
+        }
+
+        [TestMethod]
+        [DataRow("absolutelyRandomStringThatMustNotBeAValidCommand")]
+        [DataRow("create resources x y z")]
+        [DataRow("tansta resources a b")]
+        public void GetResources_ShouldThrowInvalidOperationException_WhichContainsTheStringCcommand_WhenTheInputStringRepresentsAnInvalidCommand(string command)
+        {
+            var factory = new ResourcesFactory();
+
+            Assert.ThrowsException<InvalidOperationException>(
+                () => factory.GetResources(command), "Invalid command");
+        }
+
+        [TestMethod]
+        [DataRow("create resources silver(10) gold(97853252356623523532) bronze(20)")]
+        [DataRow("create resources silver(555555555555555555555555555555555) gold(97853252356623523532999999999) bronze(20)")]
+        [DataRow("create resources silver(10) gold(20) bronze(4444444444444444444444444444444444444)")]
+        public void GetResources_ShouldThrowOverflowException_WhenTheInputStringCommandIsInTheCorrectFormat_ButAnyOfTheValueThatRepresentTheResourceAmount_IsLargerThanUintMaxValue(string command)
+        {
+            var factory = new ResourcesFactory();
+
+            Assert.ThrowsException<OverflowException>(
+                () => factory.GetResources(command));
+        }
+    }
+}
---
+++ KPK-Part-I-Unit-Test/3-Exams/01.09-August-2016/IntergalacticTravel.Test/UnitTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using IntergalacticTravel.Contracts;
+
+namespace IntergalacticTravel.Test
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Pay_ShouldThrowNullReferenceException_IfTheObjectPassedIsNull()
+        {
+            var someUnit = new Unit(1, "pesho");
+
+            Assert.ThrowsException<NullReferenceException>
+                (() => someUnit.Pay(null));
+        }
+
+        [TestMethod]
+        public void Pay_ShouldDecrease_TheOwnersAmountOfResources_ByTheAmountOfTheCost()
+        {
+            var teleportStationMocked = new Mock<ITeleportStation>();
+
+            var listOfStations =
+              new List<ITeleportStation> { teleportStationMocked.Object };
+
+            var someOwner = new BusinessOwner(1, "pesho", listOfStations);
+
+            var someCostMocked = new Mock<IResources>();
+
+            someCostMocked.Setup(
+                m => m.GoldCoins).Returns(10);
+            someCostMocked.Setup(
+                m => m.SilverCoins).Returns(10);
+            someCostMocked.Setup(
+                m => m.BronzeCoins).Returns(10);
+
+            var someResourceMocked = new Mock<IResources>();
+
+            someResourceMocked.Setup(
+              m => m.GoldCoins).Returns(100);
+            someResourceMocked.Setup(
+                m => m.SilverCoins).Returns(100);
+            someResourceMocked.Setup(
+                m => m.BronzeCoins).Returns(100);
+
+            someOwner.Resources.Add(someResourceMocked.Object);
+
+            someOwner.Pay(someCostMocked.Object);
+
+            Assert.AreEqual((uint)90,someOwner.Resources.BronzeCoins);
+            Assert.AreEqual((uint)90, someOwner.Resources.GoldCoins);
+            Assert.AreEqual((uint)90, someOwner.Resources.SilverCoins);
+        }
+
+        [TestMethod]
+        public void Pay_ShouldReturnResourceObject_WithTheAmountOfResourcesInTheCostObject()
+        {
+            var unit = new Unit(1, "Peshkata");
+
+            var someCostMocked = new Mock<IResources>();
+
+            someCostMocked.Setup(
+                m => m.GoldCoins).Returns(10);
+            someCostMocked.Setup(
+                m => m.SilverCoins).Returns(10);
+            someCostMocked.Setup(
+                m => m.BronzeCoins).Returns(10);
+
+            var espectedResource = unit.Pay(someCostMocked.Object);
+
+            Assert.AreEqual(espectedResource.BronzeCoins, 
+                someCostMocked.Object.BronzeCoins);
+            Assert.AreEqual(espectedResource.SilverCoins,
+                someCostMocked.Object.SilverCoins);
+            Assert.AreEqual(espectedResource.GoldCoins, 
+                someCostMocked.Object.GoldCoins);
+        }
+    }
+}
