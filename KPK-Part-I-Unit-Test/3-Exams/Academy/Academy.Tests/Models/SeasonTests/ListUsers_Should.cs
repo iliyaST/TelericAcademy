6c0cced0ed05196f6dd3228cec70282d646f04f7
iliyaST <iliamartinovstambolov@gmail.com>
2017-02-17 15:45:34 +0200

    Academy

---
+++ KPK-Part-I-Unit-Test/3-Exams/Academy/Academy.Tests/Models/SeasonTests/ListUsers_Should.cs
@@ -0,0 +1,55 @@
+﻿using Academy.Models;
+using Academy.Models.Contracts;
+using Academy.Models.Enums;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy.Tests.Models.SeasonTests
+{
+    [TestFixture]
+    public class ListUsers_Should
+    {
+
+        [Test]
+        public void IterateOverTheCollection_WhenTheCollectionIsNotEmpty()
+        {
+            // Arrange
+            var season = new Season(2016, 2017, Initiative.KidsAcademy);
+
+            //var studentsMock = Create<Student>();
+            //Arrange(() => studentsMock.ToString()).OccursOnce();
+
+            //season.Students.Add(studentsMock);
+
+            // Act
+            var test = season.ListUsers();
+
+            // Assert
+           
+        }
+
+        [Test]
+        public void IterateOverTheCollection_WhenTheCollectionIsNotEmpty_Moq()
+        {
+            // Arrange
+            var season = new Season(2016, 2017, Initiative.KidsAcademy);
+
+            var studentsMock = new Mock<IStudent>();
+
+            studentsMock.Setup(x=>x.ToString()).Returns("");
+
+            season.Students.Add(studentsMock.Object);
+
+            // Act
+            var test = season.ListUsers();
+
+            // Assert
+            studentsMock.Verify(x => x.ToString(), Times.Exactly(1));
+        }
+    }
+}
