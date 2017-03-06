c64c09cce317108ae8c1beb8f3795385608e9206
iliyaST <iliamartinovstambolov@gmail.com>
2017-03-06 16:23:17 +0200

    High-Quality-Code-Part-1/08-High-Quality-Classes/Homework/

---
+++ High-Quality-Code-Part-1/08-High-Quality-Classes/Homework/Abstraction/Circle.cs
@@ -0,0 +1,32 @@
+﻿namespace Abstraction
+{
+    using System;
+
+    public class Circle : Figure
+    {        
+        public Circle(double radius)
+            : base(radius, radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Radius cannot be empty or less than zero.");
+            }
+
+            this.Radius = radius;
+        }
+
+        public double Radius { get; private set; }
+
+        public override double CalcPerimeter()
+        {
+            double perimeter = 2 * Math.PI * this.Radius;
+            return perimeter;
+        }
+
+        public override double CalcSurface()
+        {
+            double surface = Math.PI * this.Radius * this.Radius;
+            return surface;
+        }
+    }
+}
---
+++ High-Quality-Code-Part-1/08-High-Quality-Classes/Homework/Cohesion-and-Coupling/UtilsExamples.cs
@@ -0,0 +1,30 @@
+﻿namespace CohesionAndCoupling
+{
+    using System;
+
+    public class UtilsExamples
+    {
+       public static void Main()
+        {
+            Console.WriteLine(FileExtensionMethods.GetFileExtension("example"));
+            Console.WriteLine(FileExtensionMethods.GetFileExtension("example.pdf"));
+            Console.WriteLine(FileExtensionMethods.GetFileExtension("example.new.pdf"));
+
+            Console.WriteLine(FileExtensionMethods.GetFileNameWithoutExtension("example"));
+            Console.WriteLine(FileExtensionMethods.GetFileNameWithoutExtension("example.pdf"));
+            Console.WriteLine(FileExtensionMethods.GetFileNameWithoutExtension("example.new.pdf"));
+
+            Console.WriteLine("Distance in the 2D space = {0:f2}", Distance.CalcDistance2D(1, -2, 3, 4));
+            Console.WriteLine("Distance in the 3D space = {0:f2}", Distance.CalcDistance3D(5, 2, -1, 3, -6, 4));
+
+            FigureGeometry.Width = 3;
+            FigureGeometry.Height = 4;
+            FigureGeometry.Depth = 5;
+            Console.WriteLine("Volume = {0:f2}", FigureGeometry.CalcVolume());
+            Console.WriteLine("Diagonal XYZ = {0:f2}", FigureGeometry.CalcDiagonalXYZ());
+            Console.WriteLine("Diagonal XY = {0:f2}", FigureGeometry.CalcDiagonalXY());
+            Console.WriteLine("Diagonal XZ = {0:f2}", FigureGeometry.CalcDiagonalXZ());
+            Console.WriteLine("Diagonal YZ = {0:f2}", FigureGeometry.CalcDiagonalYZ());
+        }
+    }
+}
