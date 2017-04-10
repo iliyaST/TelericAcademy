008da1c6b375d2752062cdad3948dc6cc5cc6433
iliyaST <iliamartinovstambolov@gmail.com>
2017-04-10 13:00:56 +0300

    Reafctored the code, made testing class change method and variables names...

---
+++ High-Quality-Code-Part-2/03. Refactoring/homework/MatrixFullFillMent.cs
@@ -0,0 +1,80 @@
+﻿namespace FillTheMatrix
+{
+    public class MatrixFullFillment
+    {
+        public static void ChangeDirection(ref int dx, ref int dy)
+        {
+            int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+            int cd = 0;
+
+            for (int count = 0; count < 8; count++)
+            {
+                if (dirX[count] == dx && dirY[count] == dy)
+                {
+                    cd = count;
+                    break;
+                }
+            }
+
+            if (cd == 7)
+            {
+                dx = dirX[0];
+                dy = dirY[0];
+                return;
+            }
+
+            dx = dirX[cd + 1];
+            dy = dirY[cd + 1];
+        }
+
+        public static bool CheckIfOutOfMatrix(int[,] arr, int x, int y)
+        {
+            int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (x + dirX[i] >= arr.GetLength(0) || x + dirX[i] < 0)
+                {
+                    dirX[i] = 0;
+                }
+
+                if (y + dirY[i] >= arr.GetLength(0) || y + dirY[i] < 0)
+                {
+                    dirY[i] = 0;
+                }
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (arr[x + dirX[i], y + dirY[i]] == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void FindZeroCell(int[,] matrix, out int foundRow, out int foundCol)
+        {
+            var matrixLength = matrix.GetLength(0);
+            foundRow = 0;
+            foundCol = 0;
+
+            for (int row = 0; row < matrixLength; row++)
+            {
+                for (int col = 0; col < matrixLength; col++)
+                {
+                    if (matrix[row, col] == 0)
+                    {
+                        foundRow = row;
+                        foundCol = col;
+                        return;
+                    }
+                }
+            }
+        }     
+    }
+}
