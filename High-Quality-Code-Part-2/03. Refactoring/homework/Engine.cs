f7848a60c9ebb068e281c4834509773f5ad166f1
iliyaST <iliamartinovstambolov@gmail.com>
2017-04-10 16:17:13 +0300

    Made posible the user to choose size of matrix.

--- High-Quality-Code-Part-2/03. Refactoring/homework/Engine.cs
+++ High-Quality-Code-Part-2/03. Refactoring/homework/Engine.cs
@@ -1,21 +1,26 @@
 ﻿namespace FillMatrixTest
 {
+    using System;
     using QuadraticMatrixCreation;
-    using MatrixActions;
+    using MatrixActions;  
 
-    public class MatrixActionsTest
+    public class Engine
     {
         public static void Main(string[] args)
         {
-            // Console.WriteLine( "Enter a positive number " );
-            // string input = Console.ReadLine(  );
-            // int n = 0;
-            // while ( !int.TryParse( input, out n ) || n < 0 || n > 100 )
-            // {
-            //    Console.WriteLine( "You haven't entered a correct positive number" );
-            //    input = Console.ReadLine(  );
-            // }
-            int dimensions = 3;
+            Console.WriteLine("Enter a positive number ");
+
+            string input = Console.ReadLine();
+
+            int matrixDimensions = 0;
+
+            while (!int.TryParse(input, out matrixDimensions) || matrixDimensions < 0 || matrixDimensions > 100)
+            {
+                Console.WriteLine("You haven't entered a correct positive number");
+                input = Console.ReadLine();
+            }
+
+            int dimensions = matrixDimensions;
 
             var matrix = new QadraticMatrix(dimensions);
 
