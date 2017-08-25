ecbffdb7a81869eb9bbc846e1d5cf800c9cc488c
iliyaST <iliamstambolov@gmail.com>
2017-08-26 00:31:32 +0300

    Comit

---
+++ DSA/Practice/Tasks/Hacked/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Businessmen
+{
+    public class Person
+    {
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public int Position { get; set; }
+
+        public string Name { get; set; }
+    }
+
+    public static void Main()
+    {
+        var people = new List<Person>();
+
+        while (true)
+        {
+            var command = Console.ReadLine().Split(' ');
+
+            if (command[0] == "End")
+            {
+                break;
+            }
+            else if (command[0] == "Append")
+            {
+                var person = new Person(command[1]);
+
+                people.Add(person);
+
+                Console.WriteLine("Ok");
+            }
+            else if (command[0] == "Serve")
+            {
+                var count = int.Parse(command[1]);
+
+                if (count > people.Count)
+                {
+                    Console.WriteLine("Error");
+                }
+                else
+                {
+                    var result = new StringBuilder();
+                    for (int i = 0; i < count; i++)
+                    {
+                        var currentPerson = people[0];
+
+                        if (i == count - 1)
+                        {
+                            result.Append(currentPerson.Name);
+                            people.Remove(currentPerson);
+
+                            Console.WriteLine(result);
+                            continue;
+                        }
+
+                        result.Append(currentPerson.Name + " ");
+                        people.Remove(currentPerson);
+                    }
+                }
+            }
+            else if (command[0] == "Find")
+            {
+                var name = command[1];
+
+                var result = people.FindAll(x => x.Name == name)
+                    .Select(x => x)
+                    .ToArray();
+
+                Console.WriteLine(result.Length);
+            }
+            else if (command[0] == "Insert")
+            {
+                var person = new Person(command[2]);
+
+                var position = int.Parse(command[1]);
+
+                if (position < 0 || position > people.Count)
+                {
+                    Console.WriteLine("Error");
+                }
+                else
+                {
+                    people.Insert(position, person);
+                    Console.WriteLine("Ok");
+                }
+            }
+        }
+    }
+}
